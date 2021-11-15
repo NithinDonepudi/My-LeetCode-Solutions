7fa2a77900bae513511d2e4efbc580a59ecae539
NithinDonepudi <51224949+NithinDonepudi@users.noreply.github.com>
2021-11-15 17:13:35 -0500

    Create Move Zeroes.cpp

---
+++ Move Zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[j]=nums[i];
+                j=j+1;
+            }
+        }
+        for(int i=j;j<nums.size();j++)
+        {
+            nums[j]=0;
+        }
+    }
+};
